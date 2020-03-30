d41693a653c14cf3f26567a7ef75fe28b74d3caa
chalermpong <s_pong_c@hotmail.com>
2020-03-30 21:15:28 +0700

    Initial Commit

---
+++ TestProgressView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProgressView
+//
+//  Created by Chalermpong Satayavibul on 30/3/2563 BE.
+//  Copyright © 2563 ltman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestProgressViewTests/TestProgressViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestProgressViewTests.swift
+//  TestProgressViewTests
+//
+//  Created by Chalermpong Satayavibul on 30/3/2563 BE.
+//  Copyright © 2563 ltman. All rights reserved.
+//
+
+import XCTest
+@testable import TestProgressView
+
+class TestProgressViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
